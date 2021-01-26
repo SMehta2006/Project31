f7b4f9a6eadf0b21c412e8079a2a06040c4e11db
SMehta2006 <75286744+SMehta2006@users.noreply.github.com>
2021-01-25 17:58:05 -0800

    Add files via upload

---
+++ p5.play-boilerplate-master 3/Plinko.js
@@ -0,0 +1,17 @@
+class Plinko{
+    constructor(x,y,r){
+        var options = {
+            isStatic: true
+        }
+        this.r = r;
+        this.body = Bodies.circle(x,y,this.r,options);
+        World.add(world,this.body);
+    }
+
+    display(){
+        var pos = this.body.position;
+        ellipseMode(RADIUS);
+        FileList("white");
+        ellipse(pos.x,pos.y,10,10);
+    }
+}
